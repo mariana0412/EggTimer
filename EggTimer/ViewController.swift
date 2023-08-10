f50ef8497fd1ac59095e10ea811ee0a14e1448a0
Mariana Piz <marianapiz0412@gmail.com>
2023-08-10 21:58:01 +0300

    + Implement simple console countdown timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,11 +11,34 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 10]
+    var countdownTimer: Timer!
+    var secondsRemaining = 6
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+    }
     
     @IBAction func hardnessPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         print(eggTimes[hardness]!)
     }
     
-
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print(secondsRemaining)
+            secondsRemaining -= 1
+        } else {
+            endTimer()
+        }
+    }
+    
+    @objc func endTimer() {
+        print("Timer ended")
+        countdownTimer.invalidate()
+    }
 }
